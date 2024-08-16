7919fe50a62ec6a6de71159370dc5b0cb8f33f64
PiotrBa <piotr.balazyk@wp.pl>
2024-08-16 20:19:57 +0100

    Code formatting

--- guards/src/main/java/com/piotrba/guards/repo/GuardsRepository.java
+++ guards/src/main/java/com/piotrba/guards/repo/GuardsRepository.java
@@ -2,6 +2,8 @@ package com.piotrba.guards.repo;
 
 import com.piotrba.guards.entity.Guard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GuardsRepository extends JpaRepository<Guard, Long> {
 }
