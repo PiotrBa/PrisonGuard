ce052714124ef75f8dcc8795c46608ab8dd26694
PiotrBa <piotr.balazyk@wp.pl>
2024-08-16 20:22:03 +0100

    Tests for the findGuardById method

---
+++ guards/src/test/java/com/piotrba/guards/serviceImplTests/GuardServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.piotrba.guards.serviceImplTests;
+
+import com.piotrba.guards.entity.Guard;
+import com.piotrba.guards.repo.GuardsRepository;
+import com.piotrba.guards.service.impl.GuardServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+public class GuardServiceImplTest {
+
+    private GuardServiceImpl guardService;
+    private GuardsRepository guardsRepository;
+    Guard guard = new Guard(1L, "John", "Doe", "123456789", "john.doe@example.com", true, true);
+
+    @BeforeEach
+    public void setUp(){
+        guardsRepository = Mockito.mock(GuardsRepository.class);
+        guardService = new GuardServiceImpl(guardsRepository);
+    }
+
+    @Test
+    public void testFindGuardById_GuardExist(){
+        when(guardsRepository.findById(1L)).thenReturn(Optional.of(guard));
+        Optional<Guard> result = guardService.findGuardById(1L);
+        assertEquals(Optional.of(guard), result);
+    }
+
+    @Test
+    public void testFindGuardById_GuardDoesNotExist(){
+        when(guardsRepository.findById(2L)).thenReturn(Optional.empty());
+        Optional<Guard> result = guardService.findGuardById(2L);
+        assertFalse(result.isPresent());
+    }
+}
