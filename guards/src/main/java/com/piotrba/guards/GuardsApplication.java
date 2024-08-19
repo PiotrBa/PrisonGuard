886386972983ff267f084a6835fb7fabf6f1d1f3
PiotrBa <piotr.balazyk@wp.pl>
2024-08-19 13:05:32 +0100

    Application startup class and pom.xml

---
+++ guards/src/main/java/com/piotrba/guards/GuardsApplication.java
@@ -0,0 +1,13 @@
+package com.piotrba.guards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuardsApplication.class, args);
+    }
+
+}
