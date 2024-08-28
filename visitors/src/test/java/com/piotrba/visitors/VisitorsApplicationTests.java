8be6e75f43417ea88fae974e36d286cc6c5a6c76
PiotrBa <piotr.balazyk@wp.pl>
2024-08-28 17:59:03 +0100

    Add new module 'visitors' with initial classes and update main pom

---
+++ visitors/src/test/java/com/piotrba/visitors/VisitorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrba.visitors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
