8989dc90c55f8615f315f5a788ef0e1dcb3d60f4
PiotrBa <piotr.balazyk@wp.pl>
2024-09-03 11:10:15 +0100

    Add new module 'prisoners' with initial classes and update main pom

---
+++ prisoners/src/main/java/com/piotrba/prisoners/PrisonersApplication.java
@@ -0,0 +1,13 @@
+package com.piotrba.prisoners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisonersApplication.class, args);
+	}
+
+}
---
+++ prisoners/src/test/java/com/piotrba/prisoners/PrisonersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrba.prisoners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
