7db3da0ad6e61b8beab4438f5032dd5897ff1aaa
PiotrBa <piotr.balazyk@wp.pl>
2024-09-17 09:56:05 +0100

    Logger for VisitorsController

--- visitors/src/main/java/com/piotrba/visitors/controller/VisitorsController.java
+++ visitors/src/main/java/com/piotrba/visitors/controller/VisitorsController.java
@@ -3,6 +3,8 @@ package com.piotrba.visitors.controller;
 import com.piotrba.visitors.entity.Visitor;
 import com.piotrba.visitors.service.VisitorService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +16,21 @@ import java.util.Optional;
 @AllArgsConstructor
 public class VisitorsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VisitorsController.class);
+
     private final VisitorService visitorsService;
 
     @GetMapping
-    public ResponseEntity<Visitor> getVisitor(@RequestParam String email){
+    public ResponseEntity<Visitor> getVisitor(@RequestParam String email) {
+        logger.info("Received request to get visitor by email: {}", email);
+
         Optional<Visitor> optional = visitorsService.findByEMail(email);
-        return optional
-                .map(visitor -> ResponseEntity.ok(visitor))
-                .orElseGet(()-> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        if (optional.isPresent()) {
+            logger.info("Visitor with email: {} found", email);
+            return ResponseEntity.ok(optional.get());
+        } else {
+            logger.warn("Visitor with email: {} not found", email);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
