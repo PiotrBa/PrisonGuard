9dd45ca76b4c823d62b89f3ab4bd8d140024bd9a
PiotrBa <piotr.balazyk@wp.pl>
2024-09-22 08:45:09 +0100

    Simplify FindGuardByIdTest by Adding Builder Annotation

--- guards/src/main/java/com/piotrba/guards/entity/Guard.java
+++ guards/src/main/java/com/piotrba/guards/entity/Guard.java
@@ -3,6 +3,7 @@ package com.piotrba.guards.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Guard {
 
     @Id
--- guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
+++ guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
@@ -1,12 +1,13 @@
 package com.piotrba.guards.serviceImplTests;
 
-import com.piotrba.guards.entity.Address;
 import com.piotrba.guards.entity.Guard;
 import com.piotrba.guards.repo.GuardsRepository;
 import com.piotrba.guards.service.impl.GuardServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
@@ -14,29 +15,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class FindGuardByIdTest {
-
-    private GuardServiceImpl guardService;
+    @Mock
     private GuardsRepository guardsRepository;
-    Guard guard = new Guard(1L, "John", "Doe", "123456789", new Address("123 Main St", "12345", "Springfield"), "john.doe@example.com", true, true);
-
-    @BeforeEach
-    public void setUp(){
-        guardsRepository = Mockito.mock(GuardsRepository.class);
-        guardService = new GuardServiceImpl(guardsRepository);
-    }
-
+    @InjectMocks
+    private GuardServiceImpl guardService;
+    public Guard guard = Guard.builder()
+            .id(1L)
+            .firstName("Andy")
+            .lastName("Smith")
+            .build();
     @Test
-    public void testFindGuardById_GuardExist(){
+    public void findGuardById_whenGuardExists_shouldReturnGuard(){
         when(guardsRepository.findById(1L)).thenReturn(Optional.of(guard));
         Optional<Guard> result = guardService.findGuardById(1L);
         assertEquals(Optional.of(guard), result);
     }
 
     @Test
-    public void testFindGuardById_GuardDoesNotExist(){
+    public void findGuardById_whenGuardDoesNotExist_shouldReturnEmptyOptional(){
         when(guardsRepository.findById(2L)).thenReturn(Optional.empty());
         Optional<Guard> result = guardService.findGuardById(2L);
         assertFalse(result.isPresent());
     }
+
 }
