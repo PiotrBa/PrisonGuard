4801b50e57a7aead0183456a4d76897996fad8eb
PiotrBa <piotr.balazyk@wp.pl>
2024-09-28 05:51:19 +0100

    Code changes in UpdatePrisonerTest

--- prisoners/src/test/java/com/piotrba/prisoners/serviceImplTests/UpdatePrisonerTest.java
+++ prisoners/src/test/java/com/piotrba/prisoners/serviceImplTests/UpdatePrisonerTest.java
@@ -5,47 +5,61 @@ import com.piotrba.prisoners.entity.ImprisonmentRigour;
 import com.piotrba.prisoners.entity.Prisoner;
 import com.piotrba.prisoners.repo.PrisonersRepository;
 import com.piotrba.prisoners.service.serviceImpl.PrisonerServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class UpdatePrisonerTest {
 
+    @Mock
     private PrisonersRepository prisonersRepository;
+
+    @InjectMocks
     private PrisonerServiceImpl prisonerService;
-
-    Prisoner prisoner = new Prisoner(1L, "John", "Doe", LocalDateTime.now(), LocalDateTime.now().plusYears(5), ImprisonmentRigour.MAXIMUM_SECURITY, new Address("123 Main St", "12345", "Springfield"));
-    Prisoner updatedPrisoner = new Prisoner(1L, "John", "Smith", LocalDateTime.now(), LocalDateTime.now().plusYears(5), ImprisonmentRigour.MAXIMUM_SECURITY, new Address("123 Main St", "12345", "Springfield"));
-
-    @BeforeEach
-    public void setUp() {
-        prisonersRepository = Mockito.mock(PrisonersRepository.class);
-        prisonerService = new PrisonerServiceImpl(prisonersRepository);
-    }
+    Prisoner prisoner = Prisoner.builder()
+            .id(1L)
+            .firstName("John")
+            .lastName("Doe")
+            .incarcerationDate(LocalDateTime.now())
+            .imprisonmentEndTime(LocalDateTime.now().plusYears(5))
+            .imprisonmentRigour(ImprisonmentRigour.MAXIMUM_SECURITY)
+            .address(new Address("123 Main St", "12345", "Springfield"))
+            .build();
+    Prisoner updatedPrisoner = Prisoner.builder()
+            .id(1L)
+            .firstName("John")
+            .lastName("Smith")
+            .incarcerationDate(LocalDateTime.now())
+            .imprisonmentEndTime(LocalDateTime.now().plusYears(5))
+            .imprisonmentRigour(ImprisonmentRigour.MAXIMUM_SECURITY)
+            .address(new Address("123 Main St", "12345", "Springfield"))
+            .build();
 
     @Test
-    public void testUpdatePrisoner_PrisonsExist() {
+    public void updatePrisoner_whenPrisonerExists_shouldUpdatePrisoner() {
         when(prisonersRepository.findById(1L)).thenReturn(Optional.of(prisoner));
-        when(prisonersRepository.save(updatedPrisoner)).thenReturn(updatedPrisoner);
-        Optional<Prisoner> result = prisonerService.updatePrisoner(1L, updatedPrisoner);
-        assertEquals(updatedPrisoner, result.get());
+        Prisoner result = prisonerService.updatePrisoner(1L, updatedPrisoner);
+        assertEquals(updatedPrisoner.getLastName(), result.getLastName());
+        assertEquals(updatedPrisoner.getFirstName(), result.getFirstName());
     }
 
     @Test
-    public void testUpdatePrisoner_PrisonerDoesNotExist() {
+    public void updatePrisoner_whenPrisonerDoesNotExist_shouldThrowException() {
         when(prisonersRepository.findById(1L)).thenReturn(Optional.empty());
         try {
             prisonerService.updatePrisoner(1L, updatedPrisoner);
-            fail("Expected an IllegalStateException to be thrown");
-        } catch (IllegalStateException e) {
-            assertEquals("Prisoner does not exists", e.getMessage());
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Prisoner does not exist", e.getMessage());
         }
     }
 }
