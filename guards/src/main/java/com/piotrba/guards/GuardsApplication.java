6521673f1dd264e857ce5e92fb6e8daba4f290f1
PiotrBa <piotr.balazyk@wp.pl>
2024-10-28 11:32:27 +0000

    Enable Feign Client in Guards application

--- guards/src/main/java/com/piotrba/guards/GuardsApplication.java
+++ guards/src/main/java/com/piotrba/guards/GuardsApplication.java
@@ -2,8 +2,10 @@ package com.piotrba.guards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GuardsApplication {
 
     public static void main(String[] args) {
