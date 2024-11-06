43827a68e289d9828c0030c47be22483fcd81018
PiotrBa <piotr.balazyk@wp.pl>
2024-11-06 17:12:27 +0000

    Add PrisonerClient Feign interface to Guards module.

---
+++ guards/src/main/java/com/piotrba/guards/client/PrisonerClient.java
@@ -0,0 +1,26 @@
+package com.piotrba.guards.client;
+
+import com.piotrba.guards.dto.PrisonerDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "prisoner-client", url = "http://localhost: 8081/prisoner")
+public interface PrisonerClient {
+
+    @GetMapping
+    List<PrisonerDTO> getAllPrisoners();
+
+    @GetMapping("/{id}")
+    PrisonerDTO getPrisonerById(@PathVariable Long id);
+
+    @PostMapping("/update?{id}")
+    PrisonerDTO updatePrisoner(@PathVariable Long id, @RequestBody PrisonerDTO prisonerDTO);
+
+    @PostMapping("/delete/{id}")
+    PrisonerDTO deletePrisoner(@PathVariable Long id);
+}
