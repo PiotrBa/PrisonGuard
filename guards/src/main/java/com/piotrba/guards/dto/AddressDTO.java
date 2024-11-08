cf09643925cd3d2cd16cd17818e5b7fd92319804
PiotrBa <piotr.balazyk@wp.pl>
2024-11-08 16:02:20 +0000

    Add AddressDTO for data transfer between Guards and Prisoners modules

---
+++ guards/src/main/java/com/piotrba/guards/dto/AddressDTO.java
@@ -0,0 +1,11 @@
+package com.piotrba.guards.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressDTO {
+
+    private String firstLine;
+    private String postCode;
+    private String City;
+}
