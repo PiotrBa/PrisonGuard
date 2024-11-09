8fafb6727acbb74ddc0aface5b6686b759a88e75
PiotrBa <piotr.balazyk@wp.pl>
2024-11-09 08:10:14 +0000

    Add ImprisonmentRigourDTO for data transfer between Guards and Prisoners modules

---
+++ guards/src/main/java/com/piotrba/guards/dto/ImprisonmentRigourDTO.java
@@ -0,0 +1,10 @@
+package com.piotrba.guards.dto;
+
+public enum ImprisonmentRigourDTO {
+
+    MINIMUM_SECURITY,
+    MEDIUM_SECURITY,
+    MAXIMUM_SECURITY,
+    SOLITARY_CONFINEMENT,
+    SUPER_MAX
+}
