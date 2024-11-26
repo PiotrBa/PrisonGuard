7492a6f7880d96ee44957a7d80ee0bfe25530ea3
PiotrBa <piotr.balazyk@wp.pl>
2024-11-26 12:05:57 +0000

    Code change in the VisitorsRepository class

--- visitors/src/main/java/com/piotrba/visitors/repo/VisitorsRepository.java
+++ visitors/src/main/java/com/piotrba/visitors/repo/VisitorsRepository.java
@@ -4,10 +4,6 @@ import com.piotrba.visitors.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface VisitorsRepository extends JpaRepository<Visitor, Long> {
-
-    Optional<Visitor> findByEmail(String email);
 }
