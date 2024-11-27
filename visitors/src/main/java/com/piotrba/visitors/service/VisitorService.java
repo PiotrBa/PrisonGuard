bbc2c5edce91a8212f6a1a687280cd2d7837f44b
PiotrBa <piotr.balazyk@wp.pl>
2024-11-27 11:25:04 +0000

    Code change in the VisitorService class

--- visitors/src/main/java/com/piotrba/visitors/service/VisitorService.java
+++ visitors/src/main/java/com/piotrba/visitors/service/VisitorService.java
@@ -2,9 +2,17 @@ package com.piotrba.visitors.service;
 
 import com.piotrba.visitors.entity.Visitor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VisitorService {
 
-    Optional<Visitor> findByEMail(String email);
+    List<Visitor> findAllVisitors();
+    Optional<Visitor> findVisitorById(Long id);
+
+    Visitor addNewVisitor(Visitor visitor);
+
+    Visitor updateVisitor(Long id, Visitor visitor);
+
+    void deleteVisitor (Long id);
 }
