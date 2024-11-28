dc1537409cea350126ce3d8a8868e2bf7de8ea18
PiotrBa <piotr.balazyk@wp.pl>
2024-11-28 12:01:51 +0000

    New methods in the VisitorServiceImpl class

--- visitors/src/main/java/com/piotrba/visitors/service/serviceImpl/VisitorServiceImpl.java
+++ visitors/src/main/java/com/piotrba/visitors/service/serviceImpl/VisitorServiceImpl.java
@@ -6,6 +6,7 @@ import com.piotrba.visitors.service.VisitorService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,7 +16,40 @@ public class VisitorServiceImpl implements VisitorService {
     private final VisitorsRepository visitorsRepository;
 
     @Override
-    public Optional<Visitor> findByEMail(String email) {
-        return visitorsRepository.findByEmail(email);
+    public List<Visitor> findAllVisitors() {
+        return visitorsRepository.findAll();
+    }
+
+    @Override
+    public Optional<Visitor> findVisitorById(Long id) {
+        return visitorsRepository.findById(id);
+    }
+
+    @Override
+    public Visitor addNewVisitor(Visitor visitor) {
+        return visitorsRepository.save(visitor);
+    }
+
+    @Override
+    public Visitor updateVisitor(Long id, Visitor newVisitor) {
+        Optional<Visitor> optionalVisitor = visitorsRepository.findById(id);
+        if (optionalVisitor.isEmpty()){
+            throw new IllegalArgumentException("Visitor is not exist");
+        }
+        Visitor existingVisitor = optionalVisitor.get();
+        existingVisitor.setId(newVisitor.getId());
+        existingVisitor.setFirstName(newVisitor.getFirstName());
+        existingVisitor.setLastName(newVisitor.getLastName());
+        existingVisitor.setPhoneNumber(newVisitor.getPhoneNumber());
+        existingVisitor.setAddress(newVisitor.getAddress());
+        existingVisitor.setEmail(newVisitor.getEmail());
+        existingVisitor.setPrisonerIdNumber(newVisitor.getPrisonerIdNumber());
+        existingVisitor.setRelationshipToPrisoner(newVisitor.getRelationshipToPrisoner());
+        return existingVisitor;
+    }
+
+    @Override
+    public void deleteVisitor(Long id) {
+        visitorsRepository.deleteById(id);
     }
 }
