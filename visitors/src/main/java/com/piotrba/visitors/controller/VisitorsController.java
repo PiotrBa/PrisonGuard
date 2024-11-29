b6c6841a2db77f32d9c50d1ab7ab2f46d9b721d8
PiotrBa <piotr.balazyk@wp.pl>
2024-11-29 13:32:27 +0000

    New methods in the VisitorsController class

--- visitors/src/main/java/com/piotrba/visitors/controller/VisitorsController.java
+++ visitors/src/main/java/com/piotrba/visitors/controller/VisitorsController.java
@@ -6,7 +6,10 @@ import com.piotrba.visitors.service.VisitorService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/visitor")
@@ -17,10 +20,38 @@ public class VisitorsController {
 
     private final VisitorService visitorsService;
 
-    @GetMapping
-    public Visitor getVisitor(@RequestParam String email) {
-        logger.info("Received request to get visitor by email: {}", email);
-        return visitorsService.findByEMail(email)
-                .orElseThrow(()-> new VisitorNotFoundException("Visitor with email " + email + " not found"));
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Visitor> getAllVisitors(){
+        logger.info("Received request to get all visitors");
+        return visitorsService.findAllVisitors();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Visitor getVisitor(@PathVariable Long id) {
+        logger.info("Received request to get visitor by id: {}", id);
+        return visitorsService.findVisitorById(id)
+                .orElseThrow(()-> new VisitorNotFoundException("Visitor with id: " + id + " not found"));
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Visitor getNewVisitor(@RequestBody Visitor visitor){
+        logger.info("Received request to register a new visitor");
+        return visitorsService.addNewVisitor(visitor);
+    }
+
+    @PostMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Visitor updateVisitor (@PathVariable Long id, @RequestBody Visitor visitor){
+        logger.info("Received request to update visitor with id: {}", id);
+        return visitorsService.updateVisitor(id, visitor);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteVisitor (@PathVariable Long id){
+        logger.info("Received request to delete visitor with id: {}", id);
+        visitorsService.deleteVisitor(id);
     }
 }
