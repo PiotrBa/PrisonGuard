bf8cf2ea0997e35e8c3c0b9822d2c48c2d356f0c
PiotrBa <piotr.balazyk@wp.pl>
2024-12-04 21:16:28 +0000

    Add VisitorClient to Guards module

---
+++ guards/src/main/java/com/piotrba/guards/client/VisitorClient.java
@@ -0,0 +1,29 @@
+package com.piotrba.guards.client;
+
+import com.piotrba.guards.dto.visitor.VisitorDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "visitor-client", url = "http://localhost:8082/visitor")
+public interface VisitorClient {
+
+    @GetMapping("/all")
+    List<VisitorDTO> getAllVisitors();
+
+    @GetMapping("/{id}")
+    VisitorDTO getVisitorById(@PathVariable Long id);
+
+    @GetMapping("/prisoner/{prisonerId}")
+    List<VisitorDTO> getVisitorsByPrisonerId(@PathVariable Long prisonerId);
+
+    @PostMapping("/add")
+    VisitorDTO addVisitor(@RequestBody VisitorDTO visitorDTO);
+
+    @PostMapping("/update/{id}")
+    VisitorDTO updateVisitor(@PathVariable Long id, @RequestBody VisitorDTO visitorDTO);
+
+    @DeleteMapping("/delete/{id}")
+    void deleteVisitor(@PathVariable Long id);
+}
