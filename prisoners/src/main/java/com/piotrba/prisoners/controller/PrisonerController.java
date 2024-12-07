b29bd66712d3f6fb3566d967a653aa53ca5f9c58
PiotrBa <piotr.balazyk@wp.pl>
2024-12-07 15:45:02 +0000

    Add @ResponseStatus to PrisonerController

--- prisoners/src/main/java/com/piotrba/prisoners/controller/PrisonerController.java
+++ prisoners/src/main/java/com/piotrba/prisoners/controller/PrisonerController.java
@@ -6,6 +6,7 @@ import com.piotrba.prisoners.service.PrisonerService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,12 +21,14 @@ public class PrisonerController {
     private final PrisonerService prisonerService;
 
     @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public List<Prisoner> getAllPrisoners(){
         logger.info("Received request to get all prisoners");
         return prisonerService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Prisoner getPrisonerById(@PathVariable Long id){
         logger.info("Received request to get prisoner by id: {}", id);
         return prisonerService.findById(id)
@@ -33,18 +36,21 @@ public class PrisonerController {
     }
 
     @PostMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Prisoner updatePrisoner(@PathVariable Long id, @RequestBody Prisoner prisoner){
         logger.info("Received request to update prisoner with id: {}", id);
         return prisonerService.updatePrisoner(id, prisoner);
     }
 
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public Prisoner addPrisoner(@RequestBody Prisoner prisoner) {
         logger.info("Received request to add a new prisoner");
         return prisonerService.addPrisoner(prisoner);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deletePrisoner(@PathVariable Long id) {
         logger.info("Received request to delete prisoner with id: {}", id);
         prisonerService.deletePrisoner(id);
