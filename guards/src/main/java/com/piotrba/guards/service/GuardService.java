56ed2c538f4783ea9ce26de3a2599f214ead064a
PiotrBa <piotr.balazyk@wp.pl>
2024-12-29 14:53:56 +0000

    Add new methods to GuardService

--- guards/src/main/java/com/piotrba/guards/service/GuardService.java
+++ guards/src/main/java/com/piotrba/guards/service/GuardService.java
@@ -2,17 +2,18 @@ package com.piotrba.guards.service;
 
 import com.piotrba.guards.dto.AssignRequest;
 import com.piotrba.guards.dto.prisoner.PrisonerDTO;
+import com.piotrba.guards.dto.visitor.VisitorDTO;
 import com.piotrba.guards.entity.Guard;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface GuardService {
-
     List<Guard> findAllGuards();
     Optional<Guard> findGuardById(Long id);
     Guard registerNewGuard(Guard guard);
     Guard updateGuard(Long id, Guard guard);
-    PrisonerDTO getPrisonerById (Long id);
+    PrisonerDTO getPrisonerById(Long id);
+    VisitorDTO getVisitorById(Long id);
     void assignPrisonerToVisitor(AssignRequest request);
 }
